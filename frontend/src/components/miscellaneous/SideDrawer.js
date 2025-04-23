6d5f41f494569793cdeb4f6cdc458e6e74177cf1
Vijay kalyan <vijaykalyan4433@gmail.com>
2025-04-23 16:25:42 +0530

    updated frontend ui

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -142,7 +142,7 @@ function SideDrawer() {
           </Button>
         </Tooltip>
         <Text fontSize="2xl" fontFamily="Work sans">
-        ChatMate ❤️
+          Talk-A-Tive
         </Text>
         <div>
           <Menu>
